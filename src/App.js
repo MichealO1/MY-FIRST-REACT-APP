4f288df4eeb671d005176a09036776ca4dae3833
core.pager=cat - <mikepool599@gmail.com>
2021-11-09 00:18:20 +0100

    my first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import { Button, Form, Col, Container, Row} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+const  LoginPage =  () => {
+      return (
+
+        <Container> 
+          <h1 className = "shadow-sm text-success mt-5 p-3 text-center rounded">Admin Login</h1>
+          <Row className="mt-5">
+            <Col lg={5} md={6} sm={6} className= "p-5 m-auto shadow-sm rounded-lg">
+                <Form>
+                      <Form.Group  controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+
+
+                      <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+
+                      <div className="d-grid gap-2">
+                        <Button  variant="success btn-block" size="lg">
+                          Login
+                        </Button>
+                      </div>
+                </Form>
+            </Col>
+          </Row>
+
+          <h6  className = "mt-5 p-5 text-center text-secondary "> Copyright &copy; 2021 Micheal Boluwatife. All Rights Reserved.</h6>
+
+        </Container>
+  )
+
+}
+
+export default LoginPage;
